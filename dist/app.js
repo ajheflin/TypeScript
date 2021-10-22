a25735f4952142e3204db1d8796c0e59c55c2e5c
ajheflin <me@ajheflin.com>
2021-10-22 02:40:57 -0400

    Updated

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,84 @@
 "use strict";
-// type AddFn = (a: number, b: number) => number;
-var add;
-add = function (n1, n2) {
-    return n1 + n2;
+var e1 = {
+    name: "AJ",
+    privileges: ["Create Server"],
+    startDate: new Date()
 };
-var Person = /** @class */ (function () {
-    function Person(name) {
-        this.name = name;
+function add(n1, n2) {
+    if (typeof n1 === "string" || typeof n2 === "string") {
+        return n1.toString() + n2.toString();
     }
-    Person.prototype.greet = function () {
-        console.log("Hello, I am " + this.name);
+    return n1 + n2;
+}
+var hw = add(5, 5);
+var fetchedUserData = {
+    id: "u1",
+    name: "AJ",
+    job: {
+        title: "Developer",
+        description: "Writes code"
+    }
+};
+console.log(fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job.title);
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    if ('privileges' in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log("Start Date: " + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("Driving");
     };
-    return Person;
+    return Car;
 }());
-var user1 = new Person("AJ");
-user1.greet();
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("Truck Driving");
+    };
+    Truck.prototype.loadCargo = function () {
+        console.log("Loading...");
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo();
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
+function moveAnimal(animal) {
+    var speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log("Moving with speed " + speed);
+}
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
+moveAnimal({ type: 'horse', runningSpeed: 10 });
+var paragraph = document.querySelector("p");
+// const input = <HTMLInputElement>document.getElementById("user-input")!;
+var input = document.getElementById("user-input");
+input.value = "Hello!";
+var errorBag = {
+    email: "Not a valid email!",
+    username: "Must start with a capital character!"
+};
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,129 @@
-// type AddFn = (a: number, b: number) => number;
+type Admin = {
+    name: string;
+    privileges: string[];
+};
 
-interface AddFn {
-    (a:number, b:number): number;
-}
+type Employee = {
+    name: string;
+    startDate: Date;
+};
 
-let add: AddFn;
+type ElevatedEmployee = Admin & Employee;
 
-add = (n1: number, n2: number) => {
+const e1: ElevatedEmployee = {
+    name: "AJ",
+    privileges: ["Create Server"],
+    startDate: new Date()
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(n1: number, n2: number): number;
+function add(n1: string, n2: string): string;
+function add(n1: Combinable, n2: Combinable) {
+    if(typeof n1 === "string" || typeof n2 === "string") {
+        return n1.toString() + n2.toString();
+    }
     return n1 + n2;
 }
 
-interface Named {
-    readonly name: string;
-}
-interface Greetable extends Named {
-    greet(): void;
-}
+const hw = add(5,5);
 
-class Person implements Greetable {
-
-    constructor(public readonly name: string) {
-    }
-
-    greet() {
-        console.log("Hello, I am " + this.name);
+const fetchedUserData = {
+    id: "u1",
+    name: "AJ",
+    job: {
+        title: "Developer",
+        description: "Writes code"
     }
 }
 
-const user1 = new Person("AJ");
+console.log(fetchedUserData?.job.title);
 
-user1.greet();
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log("Name: " + emp.name);
+    if('privileges' in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if('startDate' in emp) {
+        console.log("Start Date: " + emp.startDate);
+    }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+    drive() {
+        console.log("Driving");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Truck Driving");
+    }
+    loadCargo() {
+        console.log("Loading...");
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    if(vehicle instanceof Truck) {
+        vehicle.loadCargo();
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
+}
+interface Horse {
+    type: 'horse';
+    runningSpeed: number;
+}
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    let speed: number;
+    switch(animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log("Moving with speed " + speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 10});
+moveAnimal({type: 'horse', runningSpeed: 10});
+
+const paragraph = document.querySelector("p");
+// const input = <HTMLInputElement>document.getElementById("user-input")!;
+const input = document.getElementById("user-input")! as HTMLInputElement;
+
+input.value = "Hello!";
+
+interface ErrorContainer {
+    // id: string;
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    email: "Not a valid email!",
+    username: "Must start with a capital character!"
+};
